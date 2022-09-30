5ac07a4f6227de64d30bea84539d829790cca423
michael <michael.potyomkin@gmail.com>
2022-09-30 13:09:07 +0300

    Rename elements for clarity, add docstrings

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ import cv2
 from fastapi import FastAPI, UploadFile, Request
 from fastapi.templating import Jinja2Templates
 
-from utils import load_image_into_numpy_array, zipfiles
+from utils import image_from_bytes, zip_files, process_image
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 
 
 @app.post("/uploadfiles/")
-async def create_upload_files(request: Request, files: list[UploadFile]):
-    for i, file in enumerate(files):
+async def create_upload_files(files: list[UploadFile]):
+    for index, uploaded_file in enumerate(files):
         try:
-            contents = file.file.read()
-            image = load_image_into_numpy_array(contents)
-            _, thresh = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY)
-            cv2.imwrite(f"{i}.jpg", thresh)
+            contents = uploaded_file.file.read()  # read image bytes
+            watermarked_image = image_from_bytes(contents)  # get cv2 image from bytes
+            unwatermarked_image = process_image(watermarked_image)
+            cv2.imwrite(f"{index}.jpg", unwatermarked_image)  # write image to disk
         except Exception:
             return {"message": "There was an error uploading the file"}
         finally:
-            file.file.close()
+            uploaded_file.file.close()
 
-    return zipfiles([f"{i}.jpg" for i in range(len(files))])
+    return zip_files([f"{i}.jpg" for i in range(len(files))])
 
 
 @app.get("/")
--- utils.py
+++ utils.py
@@ -7,31 +7,51 @@ import numpy as np
 from fastapi import Response
 
 
-def load_image_into_numpy_array(data):
-    npimg = np.frombuffer(data, np.uint8)
-    frame = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-    return frame
+def process_image(watermarked_image):
+    """
+    Apply simple thresholding to an image
+    :param watermarked_image: cv2 image
+    :return: processed cv2 image
+    """
+    _, unwatermarked_image = cv2.threshold(watermarked_image, 150, 255, cv2.THRESH_BINARY)  # process image
+    return unwatermarked_image
 
 
-def zipfiles(filenames: list):
-    zip_filename = "archive.zip"
+def image_from_bytes(data):
+    """
+    Get cv2 image from bytes
+    :param data: Image bytes. For example, from request data
+    :return: cv2 image
+    """
+    encoded_image_numpy_array = np.frombuffer(data, np.uint8)
+    decoded_image = cv2.imdecode(encoded_image_numpy_array, cv2.IMREAD_COLOR)
+    return decoded_image
 
-    s = io.BytesIO()
-    zf = zipfile.ZipFile(s, "w")
 
-    for fpath in filenames:
+def zip_files(filenames: list):
+    """
+    Creates a zip file archive and returns it as a response
+    :param filenames: List of files to be zipped
+    :return: fastapi.Response object
+    """
+    zip_filename = "unwatermarked_images.zip"
+
+    bytes_buffer = io.BytesIO()
+    zipped_file = zipfile.ZipFile(bytes_buffer, "w")
+
+    for filepath in filenames:
         # Calculate path for file in zip
-        fdir, fname = os.path.split(fpath)
+        directory, filename = os.path.split(filepath)
 
         # Add file, at correct path
-        zf.write(fpath, fname)
+        zipped_file.write(filepath, filename)
 
     # Must close zip for all contents to be written
-    zf.close()
+    zipped_file.close()
 
     # Grab ZIP file from in-memory, make response with correct MIME-type
-    resp = Response(s.getvalue(), media_type="application/x-zip-compressed", headers={
+    response = Response(bytes_buffer.getvalue(), media_type="application/x-zip-compressed", headers={
         'Content-Disposition': f'attachment;filename={zip_filename}'
     })
 
-    return resp
+    return response
